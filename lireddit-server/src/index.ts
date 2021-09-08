81954cb6a1b635938e67aa6f137f9eca9ee4410f
Svetoslav Yordanov <svetlio@gmail.com>
2021-09-08 18:32:09 +0300

    Mikro ORM Entities

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello');
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: 'lireddit',
+    type: 'postgresql',
+    debug: !__prod__,
+  });
+};
+main();
